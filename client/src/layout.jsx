4ef4e18472b58b8839c872af601297c4ff76944b
amankumar322nit <akamankumar322@gmail.com>
2024-04-21 01:23:09 +0530

    footer added

--- client/src/layout.jsx
+++ client/src/layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar.jsx'
+import Footer from './components/navbar/Footer.jsx'
 
 function Layout() {
   return (
     <div>
     <Navbar/>
     <Outlet />
+    <Footer/>
     </div>
   )
 }
