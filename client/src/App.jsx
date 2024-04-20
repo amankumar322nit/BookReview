05d6054f814c04130ed23059f36c8de4d43d636d
amankumar322nit <akamankumar322@gmail.com>
2024-04-21 01:17:50 +0530

    Signup and Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Layout from "./layout.jsx";
+import { SignUp } from "./pages/SignUp.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
 
 const router = createBrowserRouter(
   createRoutesFromElements(
+    <Route>
     <Route path='/' element={<Layout />}>
     </Route>
+    <Route path='/signup' element={<SignUp/>}>
+    </Route>
+    <Route path='/login' element={<Login/>}>
+    </Route>
+    </Route>
+    
   )
 )
   return (
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,8 @@
 
 
-
-export function SignUp() {
+const Login = () => {
   return (
-<div className="flex h-screen">
+    <div className="flex h-screen">
   <div className="hidden lg:flex items-center justify-center flex-1 bg-white text-black">
     <div className="max-w-md text-center">
       <svg xmlns="http://www.w3.org/2000/svg" width="524.67004" height="531.39694" className="w-full" alt="https://undraw.co/illustrations" title="https://undraw.co/illustrations" viewBox="0 0 524.67004 531.39694" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -78,12 +77,8 @@ export function SignUp() {
   </div>
   <div className="w-full bg-gray-100 lg:w-1/2 flex items-center justify-center">
     <div className="max-w-md w-full p-6">
-      <h1 className="text-3xl font-semibold mb-6 text-black text-center">Sign Up</h1>
+      <h1 className="text-3xl font-semibold mb-6 text-black text-center">Log In</h1>
       <form action="#" method="POST" className="space-y-4">
-        <div>
-          <label for="username" className="block text-sm font-medium text-gray-700">Username</label>
-          <input type="text" id="username" name="username" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"/>
-        </div>
         <div>
           <label for="email" className="block text-sm font-medium text-gray-700">Email</label>
           <input type="text" id="email" name="email" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"/>
@@ -93,11 +88,11 @@ export function SignUp() {
           <input type="password" id="password" name="password" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"/>
         </div>
         <div>
-          <button type="submit" className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Sign Up</button>
+          <button type="submit" className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Log in</button>
         </div>
       </form>
       <div className="mt-4 text-sm text-gray-600 text-center">
-        <p>Already have an account? <a href="/login" className="text-black hover:underline">Login here</a>
+        <p>Create a new Account?<a href="/signup" className="text-black hover:underline"> Signup here</a>
         </p>
       </div>
     </div>
@@ -105,3 +100,5 @@ export function SignUp() {
 </div>
   )
 }
+
+export default Login
