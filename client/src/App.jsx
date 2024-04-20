c699b04f23370c89ffebfdf3e11b71246558f529
amankumar322nit <akamankumar322@gmail.com>
2024-04-21 00:27:29 +0530

    navbar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,19 @@
-import { useEffect, useState } from "react";
-//import { useAxios } from "./customHooks/useAxios";
-import axios from "axios";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Layout from "./layout.jsx";
 
 function App() {
-//   const { response } = useAxios({
-//     method: 'get',
-//     url: '/api/',
-//     headers: JSON.stringify({ accept: '*/*' }),
-//     body: JSON.stringify({
-//       userId: 1,
-//       id: 19392,
-//       title: 'title',
-//       body: 'Sample text',
-//   }),
-// });
-const [data, setData] = useState([]);
 
-useEffect(() => {
-    axios.get('/api').then((res)=> {setData(res.data),console.log(res.data)});
-}, []);
-  return <>
-  <div>Aman Kumar</div>
-  {data.message}</>;
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+    </Route>
+  )
+)
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+    <RouterProvider router={router} />
+    </div>
+  )
 }
 
 export default App;
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import Logo from "../../assets/website/logo.png";
+import DarkMode from "./DarkMode";
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    link: "/#services",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <div className="container py-3 sm:py-0">
+          <div className="flex justify-between items-center">
+            <div>
+              <a href="/" className="font-bold text-2xl sm:text-3xl flex gap-2">
+                <img src={Logo} alt="Logo" className="w-10" />
+                Books Review
+              </a>
+            </div>
+            <div className="flex justify-between items-center gap-4">
+              <div>
+                <DarkMode />
+              </div>
+              <ul className="hidden sm:flex items-center gap-4">
+                {Menu.map((menu) => (
+                  <li key={menu.id}>
+                    <a
+                      href={menu.link}
+                      className="inline-block py-4 px-4 hover:text-primary duration-200"
+                    >
+                      {menu.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
